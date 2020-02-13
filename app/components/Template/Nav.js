d68ce9dfe6d97c15a390af47ab97978981bd9507
VladimirNeri <vlad.neri@gmail.com>
2020-02-13 12:16:02 -0800

    added projects

--- app/components/Template/Nav.js
+++ app/components/Template/Nav.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
-
 import data from '../../data/contact';
 
 const Nav = () => (
--- app/data/projects.js
+++ app/data/projects.js
@@ -1,14 +1,6 @@
 // TODO Add a couple lines about each project
 const data = [
   {
-    title: 'TravelBud',
-    subtitle: 'One Stop Shop for Travelers',
-    link: 'https://vladimirneri.github.io/TheTravelBud/',
-    image: '/images/projects/travelbud.jpg',
-    date: '2019-05-22',
-    desc: 'A one stop shop for all your travel needs. '
-      + 'This app contains a Flight Tracker, Currency Converter, Weather Forecast, and Packing List.',
-  }, {
     title: 'My Kitchen',
     subtitle: 'An Inventory Management System for Caterers',
     link: 'https://serene-wave-45223.herokuapp.com/',
@@ -24,6 +16,46 @@ const data = [
     date: '2019-08-26',
     desc: 'ScanMatch is a full stack web application that allows users to audit their digital database '
       + 'against their physical inventory. The application is designed and prepared for SoftMouse Inc.',
+  }, {
+    title: 'TravelBud',
+    subtitle: 'One Stop Shop for Travelers',
+    link: 'https://vladimirneri.github.io/TheTravelBud/',
+    image: '/images/projects/travelbud.jpg',
+    date: '2019-05-22',
+    desc: 'A one stop shop for all your travel needs. '
+      + 'This app contains a Flight Tracker, Currency Converter, Weather Forecast, and Packing List.',
+  }, {
+    title: 'Clicky Game',
+    subtitle: 'a memory game built with React',
+    link: 'https://vladimirneri.github.io/React-Clicky-Game/',
+    image: '/images/projects/clickygame.jpg',
+    date: '2019-09-24',
+    desc: 'This is a memory game created with React. Application\'s UI is broken down into components and responds to user events. '
+     + 'Technologies used include React, JQuery, HTML, CSS, Bootstrap, NodeJs, Javascript, NPM Packages',
+  }, {
+    title: 'NYTimes Google Book Search',
+    subtitle: 'a React-based Google Books Search app',
+    link: 'https://salty-ridge-80040.herokuapp.com/',
+    image: '/images/projects/booksearch.jpg',
+    date: '2019-08-16',
+    desc: 'This is a memory game created with React. Application\'s UI is broken down into components and responds to user events. '
+     + 'Technologies used include React, NodeJs, Express, and MongoDB.',
+  }, {
+    title: 'All the News that\s Fit to Scrape',
+    subtitle: 'Mongo Scraper',
+    link: 'https://pure-depths-86569.herokuapp.com/',
+    image: '/images/projects/mongoscraper.jpg',
+    date: '2019-07-27',
+    desc: 'Mongo Scraper is a web app that lets users view and leave comments on the latest news. '
+      + 'Mongoose and Cheerio are used to scrape news from another site.',
+  }, {
+    title: 'The Ultimate Tennis Trivia Game',
+    subtitle: 'a Trivia Game that uses Javascript for the logic and JQuery to manipulate HTML',
+    link: 'https://vladimirneri.github.io/TriviaGame/',
+    image: '/images/projects/triviagame.jpg',
+    date: '2019-04-25',
+    desc: 'A trivia game that shows only one question until the player answers it or their time runs out. '
+      + 'Technologies used include Javascript, JQuery, HTML, CSS, and Bootstrap.',
   }, 
 ];
  
