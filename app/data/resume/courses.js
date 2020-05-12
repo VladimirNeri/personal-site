3b102e5ee82a9eaedb63974f27aa396a941ba372
Vlad Laptop <vlad.neri@gmail.com>
2020-05-11 21:38:54 -0700

    updated courses

--- app/data/resume/courses.js
+++ app/data/resume/courses.js
@@ -2,31 +2,32 @@ const courses = [
   {
     title: 'Full Stack Web Development Coding Bootcamp',
     link: 'http://t.cred.ly/l/357007b5-c22e-47bb-941b-1556021d72f0',
-    univerity: 'UCLA Extension',
+    university: 'UCLA Extension',
   },
   {
     title: 'The Complete Javascript Course',
     link: 'https://www.udemy.com/certificate/UC-104fad21-b75a-4a38-a61d-d1f4b48208b9/',
-    univerity: 'Udemy.com',
+    university: 'Udemy.com',
   },
   
   {
     title: 'Advanced CSS and Sass: Flexbox, Grid, Animations and More!',
     link: 'https://www.udemy.com/certificate/UC-dfbbfb52-fb80-479c-9c2e-7639fe7cc21e/',
-    univerity: 'Udemy.com',
+    university: 'Udemy.com',
   },
   {
     title: 'Modern React with Redux',
     link: 'https://www.udemy.com/certificate/UC-36a495c9-ff30-454e-9930-1de6cde89bd0/',
-    univerity: 'Udemy.com',
+    university: 'Udemy.com',
   },
   {
     title: 'NodeJs, Express, MongoDB and More',  
-    univerity: 'Udemy.com',
+    link: 'https://www.udemy.com/certificate/UC-8f63e75a-be42-47e3-a5c1-8ba8ef7eea09/',
+    university: 'Udemy.com',
   },
   {
     title: 'Algorithms and Data Structures',  
-    univerity: 'Udemy.com',
+    university: 'Udemy.com',
   },
 ];
 
