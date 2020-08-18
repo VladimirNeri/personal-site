8f95a201b1302aaeb242352b1ee86f25e80acc8d
Vlad Laptop <vlad.neri@gmail.com>
2020-08-17 17:25:39 -0700

    updated project images

--- app/data/projects.js
+++ app/data/projects.js
@@ -3,11 +3,12 @@ const data = [
   {
     title: 'Elemental',
     subtitle: 'a Content Management System built with React',
+    link: 'https://github.com/VladimirNeri/Elemental',
     image: '/images/projects/elemental.png',
     date: '2020-08',
     desc: 'Elemental is a content management system designed for artists and bloggers.'
       + ' Admin will be allowed to create and delete posts while users can like and post comments.',
   },
 ];
- 
+
 export default data;
