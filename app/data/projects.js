0cc4f566a61dab53fbc07fe0ee2db808a1d43b10
VladimirNeri <vlad.neri@gmail.com>
2020-09-22 18:12:29 -0700

    projects

--- app/data/projects.js
+++ app/data/projects.js
@@ -3,7 +3,7 @@ const data = [
   {
     title: 'Elemental',
     subtitle: 'a Subscription website built with React',
-    link: 'https://vladimirneri.github.io/elemental/',
+    link: 'https://young-retreat-41159.herokuapp.com/',
     image: '/images/projects/elemental.png',
     date: '2020-08',
     desc: 'Elemental is the demo of a subscription website built with React.  Only those who subscribe can access the content.'
