d80c551713b369310f12da076056a5e33644a1e2
VladimirNeri <vlad.neri@gmail.com>
2020-12-01 20:14:56 -0800

    update projects

--- app/data/projects.js
+++ app/data/projects.js
@@ -1,6 +1,15 @@
 // TODO Add a couple lines about each project
 const data = [
   {
+    title: 'Mainboard',
+    subtitle: 'a full-stack application build with React',
+    link: 'https://arcane-temple-04163.herokuapp.com/',
+    image: '/images/projects/mainboard.png',
+    date: '2020-11',
+    desc: 'Mainboard is a talent management application built with React.  This application helps agents organize'
+      + ' their talent\'s booking, portfolio, scouting, and casting schedule.',
+  },
+  {
     title: 'Elemental',
     subtitle: 'a Subscription website built with React',
     link: 'https://young-retreat-41159.herokuapp.com/',
