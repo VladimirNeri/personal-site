74e674c1530247243e2daaa82c2ab3d36af21c97
VladimirNeri <vlad.neri@gmail.com>
2021-06-25 17:36:21 -0700

    updated resume

--- app/data/resume/positions.js
+++ app/data/resume/positions.js
@@ -2,7 +2,7 @@
 const positions = [
   {
     company: 'Self-Employed',
-    position: 'Web Developer',
+    position: 'Software Developer',
     daterange: 'October 2019 - Present',
     points: [
     ],
@@ -12,17 +12,6 @@ const positions = [
     daterange: 'September 2009 - Present',
     points: [
       'Own and operate professional photography business, acquiring and serving new clients regularly to maintain profitability.',
-    ],
-  }, {
-    company: 'City National Bank',
-    position: 'Loan Administrator',
-    link: 'https://www.cnb.com/',
-    daterange: '2006 - 2012',
-    points: [
-      'Interacted directly with clients, legal counsel, title companies, escrow companies, insurance agencies, government agencies, and other outside lending institutions to obtain information and/or request documents.',
-      'Possessed expert knowledge of general banking operations sufficient to understand the loan function, and knowledgeable of Federal and State loan documentation regulation.',
-      'Spearheaded analysis and evaluation of loan information in order to ensure accurate loan document preparation, while implementing decision-making actions to address any concerns.',
-      'Managed department projects and assisted in training new staff in loan documentation, compliance and title review.',
     ],
   },
 ];
