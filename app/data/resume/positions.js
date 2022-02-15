0e5e04a3769c618d2df7873bf673b8e739eafa62
VladimirNeri <vlad.neri@gmail.com>
2022-02-15 00:50:29 -0800

    update

--- app/data/resume/positions.js
+++ app/data/resume/positions.js
@@ -6,13 +6,6 @@ const positions = [
     daterange: 'October 2019 - Present',
     points: [
     ],
-  }, {
-    company: 'Vladimir Neri Studios',
-    position: 'Photographer',
-    daterange: 'September 2009 - Present',
-    points: [
-      'Own and operate professional photography business, acquiring and serving new clients regularly to maintain profitability.',
-    ],
   },
 ];
 
